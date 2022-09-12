12bd240d7658286fd2b9e51f4f6903776b2cdc54
Miroslav Stanojevic <webstardev3@gmail.com>
2022-09-12 12:55:05 +0200

    form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
+
+  const validateEmail = (email: string) => {
+
+  }
+
+  const submitForm = (e: any) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+    const inputObject = Object.fromEntries(formData)
+    try {
+      const emailString = inputObject.email.toString()
+      validateEmail(emailString)
+    } catch (e) {
+      console.error(e)
+    }
+    console.log(inputObject)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(event) => submitForm(event)}>
+        <div>
+          <input
+            type="text"
+            name="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            minLength={3}
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,20}$"
+            minLength={8}
+            placeholder="password"
+          />
+        </div>
+        <div>
+          <input
+            type="tel"
+            name="phone"
+            pattern="[0-9]{9-14}"
+            placeholder="phone"
+            minLength={9}
+            maxLength={15}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
